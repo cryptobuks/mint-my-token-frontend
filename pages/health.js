b1694ba9e570c1ae1a35e6baa8582dd3fe257575
Tim Holmes-Mitra <timi.holmes.mitra@gmail.com>
2018-11-17 13:17:05 +0000

    added /health endpoing

---
+++ pages/health.js
@@ -0,0 +1,14 @@
+const Health = () => (
+  <div>
+    👍{" "}
+    <style jsx>{`
+      div {
+        text-align: center;
+        font-size: 4rem;
+        padding: 2rem;
+      }
+    `}</style>
+  </div>
+)
+
+export default Health
